3516e29a8a29eb3d2c3ccbdb89bd1cbcfdafecb6
hello-world-ttj <tijotjoseph003@gmail.com>
2024-09-05 13:45:09 +0530

    #checking

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -7,6 +7,7 @@ const instance = new Razorpay({
   key_id: process.env.RAZORPAY_ID_KEY,
   key_secret: process.env.RAZORPAY_SECRET_KEY,
 });
+  console.log("🚀 ~ RAZORPAY_ID_KEY:", process.env.RAZORPAY_ID_KEY)
 
 exports.makePayment = async (req, res) => {
   try {
