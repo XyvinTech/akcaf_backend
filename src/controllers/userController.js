0cda78a72a71c403cde1ce48b8662cfea05cb22c
hello-world-ttj <tijotjoseph003@gmail.com>
2024-09-09 17:51:58 +0530

    inactive

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -446,7 +446,7 @@ exports.getApprovals = async (req, res) => {
     }
     const { pageNo = 1, limit = 10 } = req.query;
     const skipCount = 10 * (pageNo - 1);
-    const filter = { status: { $in: ["inactive", "awaiting_payment"] } };
+    const filter = { status: "inactive" };
     const totalCount = await User.countDocuments(filter);
     const data = await User.find(filter)
       .populate("college course")
