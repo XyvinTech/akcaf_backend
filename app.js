78b12eabefb6629206fbc88e1008fde8a578c97d
hello-world-ttj <tijotjoseph003@gmail.com>
2024-12-04 11:36:53 +0530

    #limit

--- app.js
+++ app.js
@@ -55,8 +55,8 @@ app.use(express.json());
 
 //* Configure Rate Limiting
 const apiRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, //* 15 minutes
-  max: 100, //* Limit each IP to 100 requests per windowMs
+  windowMs: 30 * 60 * 1000, //* 30 minutes
+  max: 250, //* Limit each IP to 100 requests per windowMs
   message: {
     status: 429,
     message: "Too many requests from this IP, please try again later.",
