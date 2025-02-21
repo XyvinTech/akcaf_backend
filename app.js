f58d17b64895c236d9945d7d5e0036442f2d423c
hello-world-ttj <tijotjoseph003@gmail.com>
2025-02-21 11:53:21 +0530

    #refresh

--- app.js
+++ app.js
@@ -56,16 +56,16 @@ app.use(cors());
 app.use(express.json());
 
 //* Configure Rate Limiting
-const apiRateLimiter = rateLimit({
-  windowMs: 30 * 60 * 1000, //* 30 minutes
-  max: 500, //* Limit each IP to 100 requests per windowMs
-  message: {
-    status: 429,
-    message: "Too many requests from this IP, please try again later.",
-  },
-  standardHeaders: true, // *Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, //* Disable the `X-RateLimit-*` headers
-});
+// const apiRateLimiter = rateLimit({
+//   windowMs: 30 * 60 * 1000, //* 30 minutes
+//   max: 500, //* Limit each IP to 100 requests per windowMs
+//   message: {
+//     status: 429,
+//     message: "Too many requests from this IP, please try again later.",
+//   },
+//   standardHeaders: true, // *Return rate limit info in the `RateLimit-*` headers
+//   legacyHeaders: false, //* Disable the `X-RateLimit-*` headers
+// });
 
 //* Set the base path for API routes
 const BASE_PATH = `/api/${API_VERSION}`;
